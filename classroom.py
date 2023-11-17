4060cefecad20ebc47f82ba9bc4f93c852625090
Corinne <CorinneKogut@gmail.com>
2023-11-16 21:43:10 -0500

    Working on part 2

--- classroom.py
+++ classroom.py
@@ -12,6 +12,24 @@ class Classroom:
         if key not in self.instructors_dict.keys():
             self.instructors_dict[key] = inst
 
+    def remove_instructor(self, key: int):
+        del self.instructors_dict[key]
+
+    def print_instructor(self):
+        print(self.instructors_dict)
+
+    def add_student(self, std: Student):
+        key = len(self.students_dict) + 1
+
+        if key not in self.students_dict.keys():
+            self.students_dict[key] = std
+
+    def remove_student(self, key: int):
+        del self.students_dict[key]
+
+    def print_student(self):
+        print(self.students_dict)
+
 
 if __name__ == "__main__":
     p1 = Student('Minnie', 'Mouse', '09091990', AliveStatus.ALIVE)
@@ -25,4 +43,11 @@ if __name__ == "__main__":
     class1.add_instructor(i2)
 
     print(len(class1.instructors_dict))
+
+    class1.remove_instructor(2)
+
+    print(len(class1.instructors_dict))
+
+    class1.print_instructor()
+
 
